c99dce3166b8c99656dc020d45b1a51f10e4d1a5
BerserKess <kesleyrainery.21@gmail.com>
2024-11-02 13:14:35 -0300

    corrigindo

--- src/main/java/CardGame/Game.java
+++ src/main/java/CardGame/Game.java
@@ -1,7 +1,5 @@
 package CardGame;
 
-import java.util.Collections;
-
 public class Game {
     public static void main(String[] args) {
 
@@ -28,28 +26,6 @@ public class Game {
         Deck jogador2Baralho = new Deck();
         jogador2Baralho.adicionarCartas(carta3);
 
-        // EXEMPLO DE BATALHA
-        System.out.println("Iniciando a batalha entre " + jogador1.getName() + " e " + jogador2.getName() + "!");
-        realizarBatalha(jogador1, jogador2, carta1, carta3);
-    }
 
-    public static void realizarBatalha(Player jogador1, Player jogador2, MonsterCard cartaJogador1, MonsterCard cartaJogador2) {
-        System.out.println(jogador1.getName() + " escolheu " + cartaJogador1.getNome());
-        System.out.println(jogador2.getName() + " escolheu " + cartaJogador2.getNome());
-
-        // COMPARANDO ATAQUES DAS CARTAS
-        if (cartaJogador1.getAtaque() > cartaJogador2.getAtaque()) {
-            System.out.println(cartaJogador1.getNome() + " venceu a batalha!");
-            jogador2.setLifePoints(jogador2.getLifePoints() - (cartaJogador1.getAtaque() - cartaJogador2.getAtaque()));
-        } else if (cartaJogador1.getAtaque() < cartaJogador2.getAtaque()) {
-            System.out.println(cartaJogador2.getNome() + " venceu a batalha!");
-            jogador1.setLifePoints(jogador1.getLifePoints() - (cartaJogador2.getAtaque() - cartaJogador1.getAtaque()));
-        } else {
-            System.out.println("A batalha terminou em empate!");
-        }
-
-        // MOSTRANDO OS PONTOS DE VIDA APÓS A BATALHA
-        System.out.println(jogador1.getName() + " agora tem " + jogador1.getLifePoints() + " pontos de vida.");
-        System.out.println(jogador2.getName() + " agora tem " + jogador2.getLifePoints() + " pontos de vida.");
     }
 }
