27142aff0f5f90251928c646bc1896d4bf88ede3
Pedro Ribeiro <pedro_ribeiroads@outlook.com>
2024-11-02 14:52:37 -0300

    Update Battle.java

--- src/main/java/CardGame/Battle.java
+++ src/main/java/CardGame/Battle.java
@@ -1,36 +1,45 @@
 package CardGame;
 
 public class Battle {
-    public void golpe(MonsterCard atacante, MonsterCard defensor,Player jogador1, Player jogador2){
-        // CASO O DEFENSOR ESTEJA EM MODO DE DEFESA
-        if (defensor.getModo() == Mode.Defesa){
-            if (atacante.getAtaque() > defensor.getDefesa()){
-                System.out.println(defensor.nome + " foi obliterado do campo!");
+    
+    public void golpe(MonsterCard atacante, MonsterCard defensor, Player jogador1, Player jogador2) {
+        int danoAoJogador;
+        
+        if (defensor.getModo() == Mode.Defesa) {
+            if (atacante.getAtaque() > defensor.getDefesa()) {
+                System.out.println(defensor.getNome() + " foi obliterado do campo!");
                 defensor.setEstaVivo(false);
-            }
-            else if (atacante.getAtaque() == defensor.getDefesa()){
-                System.out.println(atacante.nome + " conseguiu superar a defesa de " + defensor.nome);
-            }
-            else {
-                int golpeNoJogador = atacante.getAtaque() - defensor.getDefesa();
-                System.out.println(atacante.nome + " não foi pareo para a defesa de " + defensor.nome);
-                jogador1.setPontosDeVida(jogador1.getPontosDeVida() - golpeNoJogador);
-
+            
+            } else if (atacante.getAtaque() < defensor.getDefesa()) {
+                danoAoJogador = defensor.getDefesa() - atacante.getAtaque();
+                System.out.println(atacante.getNome() + " não foi páreo para a defesa de " + defensor.getNome());
+                jogador1.setPontosDeVida(jogador1.getPontosDeVida() - danoAoJogador);
+           
+            } else {
+                System.out.println(atacante.getNome() + " conseguiu igualar a defesa de " + defensor.getNome());
+           }
+        } else { 
+            danoAoJogador = Math.abs(atacante.getAtaque() - defensor.getAtaque());
+            if (atacante.getAtaque() > defensor.getAtaque()) {
+                System.out.println(defensor.getNome() + " foi obliterado do campo e o jogador " + jogador2.getNome() + " sofreu " + danoAoJogador + " de dano.");
+                defensor.setEstaVivo(false);
+                jogador2.setPontosDeVida(jogador2.getPontosDeVida() - danoAoJogador);
+            
+            } else {
+                System.out.println(atacante.getNome() + " não conseguiu superar o ataque de " + defensor.getNome() + " e foi destruído. O jogador " + jogador1.getNome() + " sofreu " + danoAoJogador + " de dano.");
+                atacante.setEstaVivo(false);
+                jogador1.setPontosDeVida(jogador1.getPontosDeVida() - danoAoJogador);
             }
         }
-        // CASO O DEFENSOR ESTEJA EM MODO DE ATAQUE
-        else {
-            int golpeNoJogador = atacante.getAtaque() - defensor.getAtaque();
-            if (atacante.getAtaque() > defensor.getAtaque()){
-                System.out.println(defensor.nome + " foi obliterado do campo! e o jogador " + jogador1.nome + " sofreu um dano de " + golpeNoJogador);
-                defensor.setEstaVivo(false);
-                jogador2.setPontosDeVida(jogador2.getPontosDeVida() - golpeNoJogador);
-            }
-            else {
-                System.out.println(atacante.nome + " não conseguiu superar o ataque de " + defensor.nome + " e foi desttruido. O jogador1 " + jogador1.nome + "sofreu " + golpeNoJogador + " de dano." );
-                jogador1.setPontosDeVida(jogador1.getPontosDeVida() - golpeNoJogador);
-                atacante.setEstaVivo(false);
-            }
+        
+        verificarVitoria(jogador1, jogador2);
+    }
+    
+    private void verificarVitoria(Player jogador1, Player jogador2) {
+        if (jogador1.getPontosDeVida() <= 0) {
+            System.out.println(jogador2.getNome() + " venceu!");
+        } else if (jogador2.getPontosDeVida() <= 0) {
+            System.out.println(jogador1.getNome() + " venceu!");
         }
     }
 }
