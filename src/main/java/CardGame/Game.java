230a1cc0388d8592b7be3204623dd9c9ca71c07f
Pedro Ribeiro <pedro_ribeiroads@outlook.com>
2024-11-02 14:26:35 -0300

    Update Game.java

--- src/main/java/CardGame/Game.java
+++ src/main/java/CardGame/Game.java
@@ -1,42 +1,29 @@
 package CardGame;
 
+import java.util.Collections;
+
 public class Game {
     public static void main(String[] args) {
 
         // CRIANDO JOGADORES
-        Player jogador1 = new Player("Yugi", 4000, false);
-        Player jogador2 = new Player("Kaiba", 4000, false);
+        Player jogador1 = new Player("Yugi",4000,false);
+        Player jogador2 = new Player("Kaiba",4000,false);
 
         // CRIANDO CARTAS
-        MonsterCard carta1 = new MonsterCard(
-                "Mago Negro", "Um mago que usa magia negra",
-                2500, 2100, "Elfo", true, Element.Trevas);
-        carta1.setModo(Mode.Defesa);
-        MonsterCard carta2 = new MonsterCard(
-                "Guerreiro Guardião Celta", "Guerreiro Feroz das florestas",
-                1400, 1200, "Elfo", true, Element.Terra);
-        carta2.setModo(Mode.Ataque);
-        MonsterCard carta3 = new MonsterCard(
-                "Dragão Branco de Olhos Azuis", "Dragão Branco que vive no alto das montanhas nevadas",
-                3000, 2500, "Dragão", true, Element.Vento);
+        MonsterCard carta1  = new MonsterCard(
+                "Mago Negro","Um mago que usa magia negra",
+                2500,2100,"Elfo",true,Element.Trevas);
+        MonsterCard carta2  = new MonsterCard(
+                "Guerreiro Guardião Celta","Guerreiro Feroz das florestas",
+                1400,1200,"Elfo",true,Element.Terra);
+        MonsterCard carta3  = new MonsterCard(
+                "Dragão Branco de Olhos Azuis","Dragão Branco que vive no alto das montanhas nevadas",
+                3000,2500,"Dragão",true,Element.Vento);
 
         // CRIANDO BARALHOS
         Deck jogador1Baralho = new Deck();
         jogador1Baralho.adicionarCartas(carta1);
         jogador1Baralho.adicionarCartas(carta2);
-
-        Deck jogador2Baralho = new Deck();
-        jogador2Baralho.adicionarCartas(carta3);
-
-        // CRIANDO MÃO
-        Hand cartasNaMao = new Hand();
-
-        // exemplo batalha
-
-        Battle batalhar = new Battle();
-        batalhar.golpe(carta2,carta1,jogador1,jogador2);
-
-
 
     }
 }
