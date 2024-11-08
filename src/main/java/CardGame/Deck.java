d5d7a8bb9c75ecbd27faf0f6766591956ebe2d9d
joaovcar <joaovcar@gmail.com>
2024-11-08 19:58:37 -0300

    atualização com o menu funcionando para testar todas as funções do jogo. falta: questão de pontos de vida; modo de defesa e eu sei lá porra

--- src/main/java/CardGame/Deck.java
+++ src/main/java/CardGame/Deck.java
@@ -1,56 +1,50 @@
 package CardGame;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
-    private List<Card> cartas;
+    List<Card> cartas; // Lista de cartas no deck
 
-    // CONSTRUTOR
+    // Construtor da classe Deck
     public Deck() {
         this.cartas = new ArrayList<>();
     }
 
-    // GETTER
-    public List<Card> getCartas() {
-        return cartas;
-    }
+    // Método estático para criar o deck e distribuir cartas entre dois jogadores
+    public static void criarDeck(List<Card> cartasDeMonstro, Deck deck1, Deck deck2) throws IllegalArgumentException {
+        Random random = new Random();
 
-    // METODOS
-        // ADICIONAR CARTA AO DECK
-    public void add(Card carta){
-        this.cartas.add(carta);
-    }
+        // Verifica se há cartas suficientes para distribuir
+        if (cartasDeMonstro.size() < 10) {
+            throw new IllegalArgumentException("É necessário ter pelo menos 10 cartas para distribuir entre os dois jogadores.");
+        }
 
-    public static void criarDeck(List<Card> cartas, Deck deck1, Deck deck2){
-        Collections.shuffle(cartas);
+        for (int i = 0; i < 5; i++) { // Cada jogador recebe 5 cartas
+            // Adiciona uma carta aleatória ao deck do jogador 1
+            deck1.cartas.add(cartasDeMonstro.remove(random.nextInt(cartasDeMonstro.size())));
+            // Adiciona uma carta aleatória ao deck do jogador 2
+            deck2.cartas.add(cartasDeMonstro.remove(random.nextInt(cartasDeMonstro.size())));
+        }
 
-        for (int i =0; i < cartas.size(); i++){
-            if (i % 2 == 0){
-                deck1.add(cartas.get(i));
+        // Verifica se ainda há cartas no deck de monstros
+        if (!cartasDeMonstro.isEmpty()) {
+            int indiceAleatorio = random.nextInt(cartasDeMonstro.size()); // Gera um índice aleatório
+            Card cartaAleatoria = cartasDeMonstro.get(indiceAleatorio);
+            if (cartaAleatoria instanceof MonsterCard) {
+                MonsterCard cartaMonstro = (MonsterCard) cartaAleatoria; // Casting seguro
+                System.out.println("Carta aleatória: " + cartaMonstro.getNome());
+            } else {
+                System.out.println("A carta aleatória não é um MonsterCard.");
             }
-            else {
-                deck2.add(cartas.get(i));
-            }
-        }
-
-    }
-
-    public void exibirCartas(){
-        for (Card carta : cartas){
-            System.out.println(carta.nome);
+        } else {
+            System.out.println("Não há mais cartas no deck de monstros.");
         }
     }
 
-    public void embaralhar(){
-            Collections.shuffle(cartas);
-        }
-
-        public void removerCarta(){
-            cartas.remove(cartas.size() - 1);
-        }
-        public Card pegarUltimaCarta(){
-           return cartas.get(cartas.size() - 1);
-        }
+    // Método para obter o número de cartas no deck
+    public int getNumeroDeCartas() {
+        return cartas.size();
+    }
 }
